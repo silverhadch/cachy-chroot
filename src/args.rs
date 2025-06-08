a3367aca2af70af68baef88c6193ae89082b0d79
Hadi Chokr <hadichokr@icloud.com>
2025-06-08 10:43:55 +0200

    Update args.rs

--- src/args.rs
+++ src/args.rs
@@ -16,4 +16,12 @@ pub struct Args {
     /// mounted
     #[arg(long = "no-auto-mount", default_value_t = false)]
     pub no_auto_mount: bool,
+
+    /// Override default ZFS pool name (default: zpcachyos)
+    #[arg(long = "zfs-pool-name", default_value = "zpcachyos")]
+    pub zfs_pool_name: String,
+
+    /// Override default ZFS root dataset (default: ROOT/cos/root)
+    #[arg(long = "zfs-root-dataset", default_value = "ROOT/cos/root")]
+    pub zfs_root_dataset: String,
 }
